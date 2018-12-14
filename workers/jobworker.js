2c603b5d68eeab1fc2b13d2911e67124a68dc9c1
David LaBine <david.labine@providence.org>
2018-12-14 08:37:10 -0800

    Structured something like this...

---
+++ workers/jobworker.js
@@ -0,0 +1,28 @@
+const AWS = require('aws-sdk');
+const {receiveQueuedMessage} = require('../util/sqsconsumer');
+
+// Set the region 
+AWS.config.update({region: 'us-west-2'});
+// Create an SQS service object
+const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+const jobQueueUrl = "https://sqs.us-west-2.amazonaws.com/962985931788/Resterapp_q";
+
+
+module.exports.jobConsumer = async () => {
+    while (true) {
+        try {
+            let message = await receiveQueuedMessage(sqs,jobQueueUrl);
+            console.log("Received message! - ", message);
+        } catch (err) {
+            console.log("jobConsumer error!", err);
+        }
+    }
+};
+
+module.exports.startWorker = () => {
+    try {
+        jobConsumer();
+    } catch (err) {
+        console.log("startWorker error!" - err);
+    }
+}
--- workers/server.js
+++ workers/server.js
@@ -1,29 +1,5 @@
-const AWS = require('aws-sdk');
-const {receiveQueuedMessage} = require('../util/sqsconsumer');
+const worker = require('./jobworker');
 
-// Set the region 
-AWS.config.update({region: 'us-west-2'});
-// Create an SQS service object
-const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
-const jobQueueUrl = "https://sqs.us-west-2.amazonaws.com/962985931788/Resterapp_q";
+// Should config globals like AWS here, and pass into the worker.
 
-const jobConsumer = async () => {
-    while (true) {
-        try {
-            let message = await receiveQueuedMessage(sqs,jobQueueUrl);
-            console.log("Received message! - ", message);
-        } catch (err) {
-            console.log("jobConsumer error!", err);
-        }
-    }
-};
-
-function startWorker() {
-    try {
-        jobConsumer();
-    } catch (err) {
-        console.log("startWorker error!");
-    }
-}
-
-startWorker();
+worker.startWorker();
