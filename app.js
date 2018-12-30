b7c911be48a0246e4c0dec579f377314de002cce
David LaBine <davelabine@gmail.com>
2018-12-30 11:38:11 -0800

    Added passport-google-oauth and initialization

--- app.js
+++ app.js
@@ -6,6 +6,24 @@ const logger = require('./util/basic-logger');
 // logger.info(process.env);  
 
 const passport = require('passport-restify');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const User = require('./models').User;
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:8080/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+       User.findOrCreate({ googleId: profile.id }, function (err, user) {
+         return done(err, user);
+       });
+  }
+));
 
 // RESTIFY
 const restify = require('restify');
